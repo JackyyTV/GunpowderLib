0cf3543827a052d6c01a4645f3ae5daae84214a7
Jacky Scrubby <jackyytv@gmail.com>
2021-01-21 00:06:25 +0800

    1.2 Update

--- src/main/java/jackyy/gunpowderlib/helper/EnergyHelper.java
+++ src/main/java/jackyy/gunpowderlib/helper/EnergyHelper.java
@@ -7,21 +7,51 @@ import net.minecraftforge.energy.CapabilityEnergy;
 
 public class EnergyHelper {
 
+    public static final String ENERGY_NBT = "Energy";
+
     public static ItemStack setDefaultEnergyTag(ItemStack stack, int energy) {
         if (!stack.hasTag()) {
             stack.setTag(new CompoundNBT());
         }
-        stack.getOrCreateTag().putInt("energy", energy);
+        stack.getOrCreateTag().putInt(ENERGY_NBT, energy);
         return stack;
     }
 
-    public static int getEnergyStored(ItemStack stack) {
-        FEStorageCapability storage = (FEStorageCapability) stack.getCapability(CapabilityEnergy.ENERGY, null).orElse(null);
-        return storage.getEnergyStored();
+    public static int receiveEnergy(ItemStack container, int maxReceive, boolean simulate) {
+        if (!container.hasTag()) {
+            setDefaultEnergyTag(container, 0);
+        }
+        int stored = getEnergyStored(container);
+        int received = Math.min(getMaxEnergyStored(container) - stored, Math.min(getMaxEnergyStored(container) / 4, maxReceive));
+        if (!simulate) {
+            stored += received;
+            NBTHelper.setInt(container, ENERGY_NBT, stored);
+        }
+        return received;
     }
 
-    public static int getMaxEnergyStored(ItemStack stack) {
-        FEStorageCapability storage = (FEStorageCapability) stack.getCapability(CapabilityEnergy.ENERGY, null).orElse(null);
+    public static int extractEnergy(ItemStack container, int maxExtract, boolean simulate) {
+        if (container.getTag() != null && !container.getTag().hasUniqueId(ENERGY_NBT)) {
+            return 0;
+        }
+        int stored = getEnergyStored(container);
+        int extracted = Math.min(stored, Math.min(getMaxEnergyStored(container) / 4, maxExtract));
+        if (!simulate) {
+            stored -= extracted;
+            NBTHelper.setInt(container, ENERGY_NBT, stored);
+        }
+        return extracted;
+    }
+
+    public static int getEnergyStored(ItemStack container) {
+        if (container.getTag() == null || !container.getTag().hasUniqueId(EnergyHelper.ENERGY_NBT)) {
+            return 0;
+        }
+        return Math.min(NBTHelper.getInt(container, EnergyHelper.ENERGY_NBT), getMaxEnergyStored(container));
+    }
+
+    public static int getMaxEnergyStored(ItemStack container) {
+        FEStorageCapability storage = (FEStorageCapability) container.getCapability(CapabilityEnergy.ENERGY, null).orElse(null);
         return storage.getMaxEnergyStored();
     }
 
--- src/main/java/jackyy/gunpowderlib/helper/StringHelper.java
+++ src/main/java/jackyy/gunpowderlib/helper/StringHelper.java
@@ -12,8 +12,6 @@ import java.util.Optional;
 
 public class StringHelper {
 
-    public static final String ENERGY_NBT = "energy";
-
     public static IFormattableTextComponent getTierText(String modid, int tier) {
         return localize(modid, "tooltip.tier", tier);
     }
