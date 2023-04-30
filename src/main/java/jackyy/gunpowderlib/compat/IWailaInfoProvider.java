57391ec8467df36bfcfd0be1f2c04b5d04ee375d
Jacky <jackyytv@gmail.com>
2023-04-30 15:08:17 +0800

    Version bump for 1.18

--- src/main/java/jackyy/gunpowderlib/compat/IWailaInfoProvider.java
+++ src/main/java/jackyy/gunpowderlib/compat/IWailaInfoProvider.java
@@ -1,11 +1,12 @@
 package jackyy.gunpowderlib.compat;
 
-import mcp.mobius.waila.api.BlockAccessor;
+import mcp.mobius.waila.api.Accessor;
 import mcp.mobius.waila.api.IComponentProvider;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.HitResult;
 
 import java.util.List;
 
 public interface IWailaInfoProvider {
-    List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, BlockAccessor accessor, IComponentProvider config);
+    List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, Accessor<HitResult> accessor, IComponentProvider config);
 }
