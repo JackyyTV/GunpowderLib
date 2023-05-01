1f39c8b1ce256e92e268cb4120c22557f5613e2e
Jacky <jackyytv@gmail.com>
2023-05-01 17:45:16 +0800

    1.2.3 Update

--- src/main/java/jackyy/gunpowderlib/handler/EventsHandler.java
+++ src/main/java/jackyy/gunpowderlib/handler/EventsHandler.java
@@ -4,7 +4,6 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.monster.CreeperEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.common.util.FakePlayer;
-import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingSetAttackTargetEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -24,25 +23,8 @@ public class EventsHandler {
         PlayerEntity player = (PlayerEntity) event.getTarget();
         LivingEntity entity = (LivingEntity) event.getEntity();
 
-        if (entity instanceof CreeperEntity) {
-            if (player.getUniqueID().equals(UUID.fromString("38de3769-70fa-441c-89e8-67280f3068a0"))) {
-                ((CreeperEntity) entity).setAttackTarget(null);
-                entity.setRevengeTarget(null);
-            }
-        }
-    }
-
-    @SubscribeEvent
-    public void onLivingUpdate(LivingEvent.LivingUpdateEvent event) {
-        if (!(event.getEntity() instanceof LivingEntity))
-            return;
-        LivingEntity entity = (LivingEntity) event.getEntity();
-        if (entity.getAttackingEntity() == null || !(entity.getAttackingEntity() instanceof PlayerEntity) || entity.getAttackingEntity() instanceof FakePlayer)
-            return;
-        PlayerEntity player = (PlayerEntity) entity.getAttackingEntity();
-
-        if (entity instanceof CreeperEntity) {
-            if (player.getUniqueID().equals(UUID.fromString("38de3769-70fa-441c-89e8-67280f3068a0"))) {
+        if (player.getUniqueID().equals(UUID.fromString("38de3769-70fa-441c-89e8-67280f3068a0"))) {
+            if (entity instanceof CreeperEntity) {
                 ((CreeperEntity) entity).setAttackTarget(null);
                 entity.setRevengeTarget(null);
             }
