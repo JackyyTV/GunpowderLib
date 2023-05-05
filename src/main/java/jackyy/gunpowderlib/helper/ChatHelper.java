b426ab6f174e218951433997e321f5e5b75b7e08
Jacky <jackyytv@gmail.com>
2023-05-06 04:26:33 +0800

    1.19.2 Backport

--- src/main/java/jackyy/gunpowderlib/helper/ChatHelper.java
+++ src/main/java/jackyy/gunpowderlib/helper/ChatHelper.java
@@ -1,7 +1,6 @@
 package jackyy.gunpowderlib.helper;
 
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.player.Player;
 
 public class ChatHelper {
@@ -11,7 +10,7 @@ public class ChatHelper {
     }
 
     public static void msgPlayerRaw(Player player, String msg) {
-        player.displayClientMessage(new TextComponent(msg), true);
+        player.displayClientMessage(Component.literal(msg), true);
     }
 
     public static void msgPlayerRaw(Player player, Component msg) {
